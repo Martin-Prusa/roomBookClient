a1aecf4ee4e5774a2fd77aea7876067d7fcbd229
Martin Průša <martin@Martin-MBP.localdomain>
2022-02-26 15:47:12 +0100

    index.js

--- src/RoomsService.js
+++ src/RoomsService.js
@@ -1,3 +1,7 @@
 export class RoomsService {
+    #url = 'http://127.0.0.1:8080/roomBookingServer-1.0-SNAPSHOT/api'
 
+    getRooms() {
+        return fetch(this.#url+"/Rooms")
+    }
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,29 @@ window.onload = () => {
     const roomsContainer = document.querySelector('#rooms-container')
     const service = new RoomsService()
 
-    service.
+    service.getRooms().then(res => res.json()).then(rooms => {
+        rooms.forEach(room => {
+            const card = document.createElement('div')
+            card.className = 'card room-card m-2'
+
+            const cardBody = document.createElement('div')
+            cardBody.className = 'card-body'
+            cardBody.innerHTML = `
+                <h5 class="card-title">${room.title}</h5>
+                <p class="card-text">${room.description}</p>
+            `
+
+            const button = document.createElement('button')
+            button.className = 'btn btn-primary'
+            button.innerText = 'Detail'
+            button.addEventListener('click', () => {
+                document.location.href = `./pages/detail-room.html?id=${room.id}`
+            })
+
+            cardBody.appendChild(button)
+
+            card.appendChild(cardBody)
+            roomsContainer.appendChild(card)
+        })
+    })
 }
