c2f8c196162f1861ac44f02f723005c3cccfb246
Martin Průša <martin@Martin-MBP.localdomain>
2022-02-26 14:11:21 +0100

    scrpit start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-window.onload = () => {
+import {RoomsService} from "./RoomsService";
 
+window.onload = () => {
+    const roomsContainer = document.querySelector('#rooms-container')
+    const service = new RoomsService()
+
+    service.
 }
